86254cd59ca084c3037598da369a2f06952cdc76
sjt88 <simonjtweed@gmail.com>
2016-07-06 11:25:52 +0100

    removes env file

--- testenv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export DOCKER_HOST=tcp://192.168.99.100:3376
-export DOCKER_TLS=true
-export DOCKER_TLS_CERT=/home/simon/.docker/machine/machines/manager/cert.pem
-export DOCKER_TLS_KEY=/home/simon/.docker/machine/machines/manager/key.pem
-export DOCKER_TLS_CACERT=/home/simon/.docker/machine/machines/manager/ca.pem
